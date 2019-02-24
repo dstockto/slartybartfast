c3488841618c33a74d791825bfae45a23e5ea2c0
David Stockton <dstockton@i3logix.com>
2019-02-23 20:55:28 -0700

    BuildFinder determines if a build exists in the repo

---
+++ src/SlartyBartfast/Services/BuildFinder.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace SlartyBartfast\Services;
+
+use League\Flysystem\AdapterInterface;
+use SlartyBartfast\DirectoryHasher;
+use SlartyBartfast\Model\ApplicationModel;
+
+class BuildFinder
+{
+    /**
+     * @var ApplicationModel
+     */
+    private $applicationModel;
+    /**
+     * @var AdapterInterface
+     */
+    private $fileSystem;
+
+    public function __construct(ApplicationModel $applicationModel, AdapterInterface $fileSystem)
+    {
+        $this->applicationModel = $applicationModel;
+        $this->fileSystem       = $fileSystem;
+    }
+
+    public function isBuildNeeded(): bool
+    {
+        $hasher = new DirectoryHasher(
+            $this->applicationModel->getRoot(),
+            $this->applicationModel->getDirectories()
+        );
+        $namer  = new ArtifactNamer($this->applicationModel, $hasher->getHash());
+
+        return !$this->fileSystem->has($namer->getArtifactName());
+    }
+}
