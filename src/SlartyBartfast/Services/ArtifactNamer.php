9e5c0f50b117cdfb3f8063b9bfda55a4569b5084
David Stockton <dstockton@i3logix.com>
2019-02-23 18:32:52 -0700

    Add artifact namer server

---
+++ src/SlartyBartfast/Services/ArtifactNamer.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace SlartyBartfast\Services;
+
+use SlartyBartfast\Model\ApplicationModel;
+
+class ArtifactNamer
+{
+    private $hash;
+    private $application;
+
+    /**
+     * ArtifactNamer constructor.
+     *
+     * @param ApplicationModel $applicationModel
+     * @param string           $hash
+     */
+    public function __construct(ApplicationModel $applicationModel, string $hash)
+    {
+        $this->application = $applicationModel;
+        $this->hash        = $hash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApplicationName(): string
+    {
+        return $this->application->getName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getArtifactName(): string
+    {
+        return strtolower(
+            implode('',
+                [
+                    $this->application->getArtifactPrefix(),
+                    '-',
+                    $this->hash,
+                    '.zip'
+                ]
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getArtifactName();
+    }
+}
