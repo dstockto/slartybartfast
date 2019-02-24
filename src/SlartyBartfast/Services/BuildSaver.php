2b7eaf98d48d53abf738cbcb4a514a1d880c1c23
David Stockton <dstockton@i3logix.com>
2019-02-23 23:16:19 -0700

    Service to save build artifacts
    It zips and stores - maybe too much

---
+++ src/SlartyBartfast/Services/BuildSaver.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace SlartyBartfast\Services;
+
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Config;
+use SlartyBartfast\DirectoryHasher;
+use SlartyBartfast\Model\ApplicationModel;
+
+class BuildSaver
+{
+    /**
+     * @var ApplicationModel
+     */
+    private $application;
+    /**
+     * @var AdapterInterface
+     */
+    private $filesystem;
+
+    public function __construct(ApplicationModel $application, AdapterInterface $filesystem)
+    {
+        $this->application = $application;
+        $this->filesystem  = $filesystem;
+    }
+
+    public function saveBuild()
+    {
+        $currentDir = getcwd();
+
+        $namer = new ArtifactNamer(
+            $this->application,
+            (new DirectoryHasher(
+                $this->application->getRoot(),
+                $this->application->getDirectories()
+            ))->getHash()
+        );
+
+        $artifactName = $namer->getArtifactName();
+
+        // zip the output directory
+        chdir($this->application->getOutputDirectory());
+        $command = "zip -r $artifactName .";
+        shell_exec($command);
+
+        $archiveFile = fopen($artifactName, 'rb');
+        // transfer that zip to the filesystem with the name provided
+        $this->filesystem->writeStream($artifactName, $archiveFile, new Config());
+
+        // Remove local zip file
+        unlink($artifactName);
+        chdir($currentDir);
+    }
+}
