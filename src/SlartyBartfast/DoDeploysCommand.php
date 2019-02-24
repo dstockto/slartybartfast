b70c53c43d706550c1648109af0d7ce25c879b61
David Stockton <dstockton@i3logix.com>
2019-02-24 00:30:42 -0700

    Added do-deploy command

--- src/SlartyBartfast/DoDeploysCommand.php
+++ src/SlartyBartfast/DoDeploysCommand.php
@@ -3,10 +3,15 @@ declare(strict_types=1);
 
 namespace SlartyBartfast;
 
+use SlartyBartfast\Model\ApplicationModel;
+use SlartyBartfast\Services\ArtifactConfig;
+use SlartyBartfast\Services\BuildDeployer;
+use SlartyBartfast\Services\FlySystemFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DoDeploysCommand extends Command
 {
@@ -14,9 +19,35 @@ class DoDeploysCommand extends Command
     {
         $output->writeln('Implement Do Deploys');
 
-        // load configuration
-        // load applications (filter if needed)
+        $config = $input->getOption('config');
+
+        if (!file_exists($config)) {
+            throw new \RuntimeException('Provided artifacts.json file does not exist');
+        }
+
+        $io                = new SymfonyStyle($input, $output);
+        $applicationConfig = new ArtifactConfig($input->getOption('config'));
+
+        $filesystem = FlySystemFactory::getAdapter(
+            $applicationConfig->getRepositoryConfig()
+        );
+
+        // Get application list (filtered)
+        $applications = $applicationConfig->getApplicationList($input->getOption('filter'));
+
         // calculate hash->archive name, download archive, extract to configured location
+        $deployers = $applications->map(
+            function (ApplicationModel $app) use ($filesystem) {
+                return new BuildDeployer($app, $filesystem);
+            }
+        );
+
+        $deployers->each(
+            function (BuildDeployer $deployer) use ($output) {
+                $deployer->deploy($output);
+            }
+        );
+
         // if archive isn't found, error
     }
 
---
+++ src/SlartyBartfast/Services/BuildDeployer.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace SlartyBartfast\Services;
+
+use League\Flysystem\AdapterInterface;
+use SlartyBartfast\Model\ApplicationModel;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BuildDeployer
+{
+    /**
+     * @var ApplicationModel
+     */
+    private $application;
+    /**
+     * @var AdapterInterface
+     */
+    private $filesystem;
+
+    /**
+     * BuildDeployer constructor.
+     *
+     * @param ApplicationModel $application
+     * @param AdapterInterface $filesystem
+     */
+    public function __construct(ApplicationModel $application, AdapterInterface $filesystem)
+    {
+        $this->application = $application;
+        $this->filesystem  = $filesystem;
+    }
+
+    public function deploy(OutputInterface $output)
+    {
+        $currentDir = getcwd();
+        chdir($this->application->getRoot());
+        // determine archive name
+        $namer = new ArtifactNamer(
+            $this->application,
+            (new DirectoryHasher(
+                $this->application->getRoot(),
+                $this->application->getDirectories()
+            ))->getHash()
+        );
+
+        // check if it exists
+        $finder = new BuildFinder($this->application, $this->filesystem);
+
+        if ($finder->isBuildNeeded()) {
+            chdir($currentDir);
+            throw new \RuntimeException('Missing build ' . $namer->getArtifactName());
+        }
+
+        $output->writeln('Found artifact ' . $namer . ' for ' . $this->application->getName());
+
+        if (!file_exists($this->application->getDeployLocation())) {
+            if (!mkdir($concurrentDirectory = $this->application->getDeployLocation(), 0755, true) && !is_dir($concurrentDirectory)) {
+                throw new \RuntimeException(
+                    sprintf('Directory "%s" was not created', $concurrentDirectory)
+                );
+            }
+        }
+
+        // download it
+        chdir($this->application->getDeployLocation());
+        file_put_contents($namer->getArtifactName(), $this->filesystem->read($namer->getArtifactName()));
+
+        // unzip it
+        shell_exec("unzip {$namer->getArtifactName()}");
+
+        unlink($namer->getArtifactName());
+
+        chdir($currentDir);
+    }
+}
