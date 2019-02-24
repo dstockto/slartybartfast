c1fe01728c9fde85a83858573d6b92d6f7f4cf42
David Stockton <dstockton@i3logix.com>
2019-02-24 01:49:07 -0700

    Actually implement the s3 factory

--- src/SlartyBartfast/Services/FlySystemS3Factory.php
+++ src/SlartyBartfast/Services/FlySystemS3Factory.php
@@ -3,28 +3,33 @@ declare(strict_types=1);
 
 namespace SlartyBartfast\Services;
 
+use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 
 class FlySystemS3Factory
 {
     public static function build(array $options): AwsS3Adapter
     {
-//        $client = S3Client::factory([
-//            'credentials' => [
-//                'key'    => 'your-key',
-//                'secret' => 'your-secret',
-//            ],
-//            'region' => 'your-region',
-//            'version' => 'latest|version',
-//        ]);
-//
-//        $adapter = new AwsS3Adapter($client, 'your-bucket-name', 'optional/path/prefix');
-//
-//        $filesystem = new Filesystem($adapter);
+        if (!isset(
+            $options['key'],
+            $options['secret'],
+            $options['region'],
+            $options['bucket-name']
+        )) {
+            throw new \RuntimeException(
+                'Missing required S3 configuration keys: key, secret, region and/or bucket-name'
+            );
+        }
 
-        // Default this?
-        //    'version' => 'latest|version',
+        $client = S3Client::factory([
+            'credentials' => [
+                'key'    => $options['key'],
+                'secret' => $options['secret'],
+            ],
+            'region' => $options['region'] ?? null,
+            'version' => 'latest',
+        ]);
 
-        throw new \RuntimeException('I have not yet created the S3 adapter factory');
-    }
+        return new AwsS3Adapter($client, $options['bucket-name'], $options['path-prefix'] ?? '');
+     }
 }
