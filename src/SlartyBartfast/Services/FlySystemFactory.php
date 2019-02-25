d76bbdec0a32e9ba3304c2b10e0737bf460632ad
David Stockton <dstockton@i3logix.com>
2019-02-24 22:27:24 -0700

    Add error messages for missing repo config parts

--- src/SlartyBartfast/Services/FlySystemFactory.php
+++ src/SlartyBartfast/Services/FlySystemFactory.php
@@ -9,6 +9,18 @@ class FlySystemFactory
 {
     public static function getAdapter(array $configuration): AdapterInterface
     {
+        if (!array_key_exists('adapter', $configuration)) {
+            throw new \RuntimeException(
+                'The configuration "repository" section is missing the "adapter" key'
+            );
+        }
+
+        if (!array_key_exists('options', $configuration)) {
+            throw new \RuntimeException(
+                'The configuration "repository" section is missing the "options" key'
+            );
+        }
+
         switch (strtolower($configuration['adapter'])) {
             case 'local':
                 return FlySystemLocalFactory::build($configuration['options']);
